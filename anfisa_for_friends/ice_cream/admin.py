22f98598eed56a998af7dfba5f58a92def6a63ab
Pavel Ulakin <ulakin.83@gmail.com>
2023-10-07 22:51:29 +1000

    lapa

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+
+admin.site.register(IceCream, IceCreamAdmin)
